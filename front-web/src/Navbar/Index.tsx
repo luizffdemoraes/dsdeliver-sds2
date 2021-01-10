d4696e77e0e84c6eadb27f085b2259bb919f63ec
luizffdemoraes <lffm1994@gmail.com>
2021-01-09 23:42:39 -0300

    Add Footer

--- front-web/src/Navbar/Index.tsx
+++ front-web/src/Navbar/Index.tsx
@@ -1,10 +1,10 @@
 import './styles.css';
-import { ReactComponent as Logo, ReactComponent } from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
 
 function Navbar() {
   return (
     <nav className="main-navbar">
-      <Logo/>
+      <Logo />
       <a href="home" className="logo-text">DS Delivery</a>
     </nav>
   )
