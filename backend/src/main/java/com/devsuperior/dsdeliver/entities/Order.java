65c5e99805f7d63d0efc148125242d6c8d964ebd
Luiz Moraes <lffm1994@gmail.com>
2021-01-11 09:43:02 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -104,6 +104,16 @@ public class Order implements Serializable{
 	public OrderStatus getStatus() {
 		return status;
 	}
+	
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
+	}
+	
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
